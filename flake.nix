9bab5dbdea58080d550a1e91e951878cb54a3f2d
xian <xian@mintchaos.com>
2026-04-28 00:39:04 -0700

    systemd: add user service, flake.nix, and home-manager module

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "MPRIS bridge for Cider music player";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "cider-mpris";
+          version = "0.1.0";
+          src = self;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+          packages = with pkgs; [ cargo rustc rust-analyzer pkg-config openssl ];
+        };
+      }
+    ) // {
+      homeModules.cider-mpris = import ./home-module.nix;
+    };
+}
---
+++ home-module.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+let cfg = config.services.cider-mpris;
+in {
+  options.services.cider-mpris = {
+    enable = lib.mkEnableOption "Cider MPRIS bridge";
+
+    package = lib.mkPackageOption pkgs "cider-mpris" { };
+
+    rpcTokenFile = lib.mkOption {
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+      description = "Path to file containing CIDER_RPC_TOKEN";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [ cfg.package ];
+
+    xdg.configFile."cider-mpris/env".text =
+      if cfg.rpcTokenFile != null
+      then "CIDER_RPC_TOKEN=${builtins.readFile cfg.rpcTokenFile}"
+      else "# Add your CIDER_RPC_TOKEN here\nCIDER_RPC_TOKEN=";
+
+    systemd.user.services.cider-mpris = {
+      Unit = {
+        Description = "Cider MPRIS Bridge";
+        After = "graphical-session.target";
+        Wants = "graphical-session.target";
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = "${cfg.package}/bin/cider-mpris";
+        EnvironmentFile = "${config.xdg.configHome}/cider-mpris/env";
+        Restart = "on-failure";
+        RestartSec = 5;
+      };
+      Install.WantedBy = [ "default.target" ];
+    };
+  };
+}
